18c26f91045fcec50eb24902668ec80856b0a572
Ariel Vallejos Torrico <ArielVallejos@Fundacion-jala.local>
2016-08-03 17:37:54 -0400

    Re organization and task of day 2

---
+++ Ariel/Day02/Class/PrintvsPuts.rb
@@ -0,0 +1,14 @@
+# puts "Primera"
+puts "Primera"
+puts "Primera"
+puts "Primera"
+
+print "Segunda"
+# print "Segunda"
+print "Segunda"
+
+=begin
+	Multiline comment
+	rescue Exception => e
+	Ruby
+=end
